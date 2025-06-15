d6a2807beeadd328b013d7b077410dadb2c3eab7
Dan <danichang.dg@gmail.com>
2025-06-15 16:27:55 -0300

    set github actions yaml

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 let requested_env = process.env.ENV || 'dev';
 
+console.log(`My requested_env: ${requested_env}`);
+
 let BASE_URL: string | undefined;
 
 if(requested_env === 'dev'){
@@ -12,10 +14,14 @@ if(requested_env === 'dev'){
     BASE_URL = process.env.ENV_DEV;
 }
 else if(requested_env === 'qa'){
+
     BASE_URL = process.env.ENV_QA;
+
 }
 else if(requested_env === 'prd'){
+
     BASE_URL = process.env.ENV_PRD;
+
 }
 
 if (BASE_URL === undefined) throw new Error(`BASE URL is not defined for requested environment: ${requested_env}`);
