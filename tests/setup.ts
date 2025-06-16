6c6cb5be3e5d8e0c558819288c564d37432a343c
Dan <danichang.dg@gmail.com>
2025-06-16 11:18:20 -0300

    log BASE_URL

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,8 +5,6 @@ dotenv.config();
 
 let requested_env = process.env.ENV || 'dev';
 
-console.log(`My requested_env: ${requested_env}`);
-
 let BASE_URL: string | undefined;
 
 if(requested_env === 'dev'){
@@ -23,6 +21,8 @@ else if(requested_env === 'prd'){
     BASE_URL = process.env.ENV_PRD;
 
 }
+
+console.log(`The BASE_URL is: ${BASE_URL}`);
 
 if (BASE_URL === undefined) throw new Error(`BASE URL is not defined for requested environment: ${requested_env}`);
 
