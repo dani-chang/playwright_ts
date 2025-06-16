a2da7ac66d5dceda74f62ad37e4aad1b0dd05a83
Dan <danichang.dg@gmail.com>
2025-06-16 18:31:36 -0300

    adding logs to check env setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,22 +5,26 @@ dotenv.config();
 
 let requested_env = process.env.ENV || 'dev';
 
+console.log(`********** Request_env es: ${requested_env}`);
+
 let BASE_URL: string | undefined;
 
 if(requested_env === 'dev'){
-
+    console.log(`********** Entro en dev`);
     BASE_URL = process.env.ENV_DEV;
 }
 else if(requested_env === 'qa'){
-
+    console.log(`********** Entro en qa`);
     BASE_URL = process.env.ENV_QA;
 
 }
 else if(requested_env === 'prd'){
-
+    console.log(`********** Entro en prd`);
     BASE_URL = process.env.ENV_PRD;
 
 }
+
+console.log(`********** EL BASE_URL ES: ${BASE_URL}`);
 
 if (BASE_URL === undefined) throw new Error(`BASE URL is not defined for requested environment: ${requested_env}`);
 
